a2f8250173049b5f3280595bcff18f31d3758f5e
RadinTiholov <radin.tiholov@mail.bg>
2021-04-10 22:43:00 +0300

    Solved the 1st problem

---
+++ Stacks and Queues - Lab/Stacks and Queues - Lab/Reverse Strings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Stacks_and_Queues___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                stack.Push(input[i]);
+            }
+            stack.Reverse();
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
