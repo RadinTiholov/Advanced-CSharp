f514b6542d58f03c8bd4b8a3f33d64a6a8601684
RadinTiholov <radin.tiholov@mail.bg>
2021-05-01 15:05:03 +0300

    Solved the first 2 problems

---
+++ Basic Algorithms/Basic Algorithms/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Basic_Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int sum = Recursion(arr, 0);
+            Console.WriteLine(sum);
+
+        }
+
+        private static int Recursion(int[] arr, int index)
+        {
+            if (index == arr.Length -1)
+            {
+                return arr[index];
+            }
+            return arr[index] + Recursion(arr, index + 1);
+        }
+    }
+}
---
+++ Basic Algorithms/Recursive Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Recursive_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorial(n));
+        }
+
+        private static int Factorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            return n * Factorial(n - 1);
+        }
+    }
+}
