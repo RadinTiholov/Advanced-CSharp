da1405c621f850020ae05358a62ce5c5a68987b3
RadinTiholov <radin.tiholov@mail.bg>
2021-05-21 14:05:03 +0300

    solved 7

--- Exercise Stacks and Queues/7. Truck Tour/Program.cs
+++ Exercise Stacks and Queues/7. Truck Tour/Program.cs
@@ -9,13 +9,38 @@ namespace _7._Truck_Tour
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
+            Queue<int[]> queue = new Queue<int[]>();
+
+            int count = 0;
+
             for (int i = 0; i < n; i++)
             {
                 int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
-                int amount = input[0];
-                int distance = input[1];
+                queue.Enqueue(input);
+            }
+            while (true)
+            {
+                int totalFuel = 0;
+                foreach (int[] petrolPump in queue)
+                {
+                    int petrol = petrolPump[0];
+                    int distance = petrolPump[1];
+                    totalFuel += petrol;
+                    totalFuel -= distance;
+                    if (totalFuel < 0)
+                    {
+                        count++;
+                        queue.Enqueue(queue.Dequeue());
+                        break;
+                    }
+                }
+                if (totalFuel >= 0)
+                {
+                    break;
+                }
 
             }
+            Console.WriteLine(count);
         }
     }
 }
