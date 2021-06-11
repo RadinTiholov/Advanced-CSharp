5717488d003abc8cb13370ee034e3eadb3b59b07
RadinTiholov <radin.tiholov@mail.bg>
2021-06-11 20:32:22 +0300

    solved all problems

---
+++ Generics - Lab/GenericScale/EqualityScale.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+        public T Left { get; set; }
+        public T Right { get; set; }
+        public EqualityScale(T left, T right)
+        {
+            this.Left = left;
+            this.Right = right;
+        }
+        public bool AreEqual() 
+        {
+            if (Left.Equals(Right))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Generics - Lab/Generics - Lab/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public Box()
+        {
+            stack = new Stack<T>();
+
+        }
+
+        private Stack<T> stack;
+        private int count;
+
+        public int Count
+        {
+            get { return stack.Count; }
+        }
+        
+        public void Add(T element) 
+        {
+            stack.Push(element);
+        }
+        public T Remove()
+        {
+            return stack.Pop();
+        }
+
+    }
+}
