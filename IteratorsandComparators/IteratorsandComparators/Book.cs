55b86da90a1eccda41080a3a9af3e952e812f878
RadinTiholov <radin.tiholov@mail.bg>
2021-06-16 21:49:20 +0300

    all prolems

---
+++ IteratorsandComparators/IteratorsandComparators/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book: IComparable<Book>
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = new List<string>(authors);
+            
+        }
+        public string Title { get; private set; }
+        public int Year { get; private set; }
+        public List<string> Authors { get; set; }
+
+        public int CompareTo(Book other)
+        {
+            if (Year != other.Year)
+            {
+                return Year.CompareTo(other.Year);
+            }
+
+            return Title.CompareTo(other.Title);
+        }
+        public override string ToString()
+        {
+            return $"{Title} {Year}";
+        }
+    }
+}
---
+++ IteratorsandComparators/IteratorsandComparators/Library.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library: IEnumerable<Book>
+    {
+        public Library(params Book[] books)
+        {
+            Books = new SortedSet<Book>(books, new BookComparator());
+        }
+        private readonly SortedSet<Book> Books;
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(Books);//////
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        //
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            public LibraryIterator(IEnumerable<Book> book)
+            {
+                Reset();
+                this.books = new List<Book>(book);
+            }
+            public Book Current => books[currentIndex];
+            private readonly List<Book> books;
+            private int currentIndex;
+
+
+            object IEnumerator.Current => this.Current;
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext() => ++currentIndex < books.Count;
+
+
+            public void Reset()
+            {
+                currentIndex = -1;
+            }
+        }
+    }
+    
+}
